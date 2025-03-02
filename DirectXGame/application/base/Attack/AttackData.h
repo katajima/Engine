fafc966d25818478af9159eb82d034014209de51
katajima <k023g1078@g.neec.ac.jp>
2025-03-02 18:32:58 +0900

    一旦

--- DirectXGame/application/base/Attack/AttackData.h
+++ DirectXGame/application/base/Attack/AttackData.h
@@ -2,19 +2,55 @@
 #include<cstdint>
 #include<string>
 #include<vector>
+#include"DirectXGame/engine/struct/Vector2.h"
+#include"DirectXGame/engine/struct/Vector3.h"
+#include"DirectXGame/engine/struct/Vector4.h"
 
 
 
-// 攻撃関係データ
+// 攻撃キーInput
+struct AttackKeyFlag
+{
+    bool IsAttack; // 攻撃するか
+
+    bool IsNormalAttack; // B
+    bool IsDashAttack;   // X
+    bool IsJampAttack;   // Y
+    bool IsSpecialAttack;// RT
+};
+
+// 攻撃インプット
+struct AttackInput
+{
+    int comboNum; // 攻撃回数
+    bool comboNext = false;
+    AttackKeyFlag keyFlag;
+};
+
+
+// 攻撃内部データ
 struct AttackData {
     std::string name;        // 技名
     float damage;            // ダメージ
-    int startupFrames;       // 発生フレーム
-    int activeFrames;        // 持続フレーム
-    int recoveryFrames;      // 硬直フレーム
+    float hitNum;            // ヒット回数
+    float startupFrames;     // 発生フレーム
+    float activeFrames;      // 持続フレーム
+    float recoveryFrames;    // 硬直フレーム
     bool canCancelOnHit;     // ヒット時にキャンセル可能か
     bool canCancelOnWhiff;   // 空振り時でもキャンセル可能か
+    AttackMotion motion;     // 攻撃モーション
 };
+
+// 攻撃モーション
+struct AttackMotion
+{
+    Vector3 rotate;            // 回転
+    Vector3 scale;             // 拡縮 
+    Vector3 translate;         // 位置 
+
+    Vector3 roll; // 回転量(フレーム)
+};
+
 
 // 攻撃技
 enum class AttackState {
@@ -24,6 +60,18 @@ enum class AttackState {
     HeavyAttack,
     SpecialAttack
 };
+
+// 攻撃タイプ(一振りの攻撃に対して)
+enum class AttackType
+{
+    Blow,               // 一撃
+    ConsecutiveHits,    // 連撃
+    Duration,           // 持続
+    LastBlow,           // 後に一撃
+    LastConsecutiveHits,// 後に連撃
+};
+
+
 
 struct ComboTransition {
     AttackState current;
